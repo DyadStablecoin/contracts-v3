12b4aed4bf0118163ac04bf74722c89f3064c816
shafu.eth <selfouly@gmail.com>
2023-02-04 21:34:54 +0100

    ERC20

--- src/core/ERC20Relative.sol
+++ src/core/ERC20Relative.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "./ERC20.sol";
 
 contract ERC20Relative is ERC20 {
   constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
@@ -13,6 +13,39 @@ contract ERC20Relative is ERC20 {
     returns (uint256) 
   {
     return super.balanceOf(account) * totalSupply();
+  }
+
+  function _transfer(address sender, address recipient, uint256 amount) 
+    internal 
+    override 
+  {
+    super._transfer(sender, recipient, amount / totalSupply());
+  }
+  
+  function _approve(address owner, address spender, uint256 amount) 
+    internal 
+    override 
+  {
+    super._approve(owner, spender, amount / totalSupply());
+  }
+
+  function _spendAllowance(address owner, address spender, uint256 amount) 
+    internal 
+    override 
+  {
+    super._spendAllowance(owner, spender, amount / totalSupply());
+  }
+
+  function _mint(uint256 amount) 
+    internal 
+  {
+    _totalSupply += amount;
+  }
+
+  function _burn(uint256 amount) 
+    internal 
+  {
+    _totalSupply -= amount;
   }
 
 }
