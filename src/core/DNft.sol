afeffdf2812ff8cee0aee363bcb68eb29c0a3bee
shafu.eth <selfouly@gmail.com>
2023-02-07 00:08:23 +0100

    rm console

--- src/core/DNft.sol
+++ src/core/DNft.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-import "forge-std/console.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import {ERC721, ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
