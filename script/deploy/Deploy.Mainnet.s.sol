f6318192f106a8c187b83f3b20b06477625dd8a2
shafu.eth <selfouly@gmail.com>
2023-02-09 20:14:47 +0100

    Deploy.Mainnet

---
+++ script/deploy/Deploy.Mainnet.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17;
+
+import "forge-std/Script.sol";
+import {DeployBase} from "./DeployBase.s.sol";
+import {Parameters} from "../../src/Parameters.sol";
+
+contract DeployMainnet is Script, Parameters {
+  function run() public {
+      new DeployBase().deploy(
+        MAINNET_ORACLE,
+        MAINNET_MIN_MINT_DYAD_DEPOSIT, 
+        MAINNET_OWNER
+      );
+  }
+}
+
