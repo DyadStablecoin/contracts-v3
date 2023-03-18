c648ee37740d820260938b2f90a9e5c0e785558e
shafu.eth <selfouly@gmail.com>
2023-03-18 13:44:34 +0100

    deploy

--- src/core/Factory.sol
+++ src/core/Factory.sol
@@ -5,11 +5,19 @@ import {DNftERC20} from "./DNftERC20.sol";
 import {Dyad} from "./Dyad.sol";
 
 contract Factory {
+
+  event Deployed(address dNft, address dyad);
+
+  // token => oracle => deployed
+  mapping(address => mapping(address => bool)) public deployed;
+
   function deploy(
     address _token, 
     address _oracle,
     string memory _flavor 
   ) public {
+    require(!deployed[_token][_oracle]);
+
     Dyad dyad = new Dyad(
       string.concat("DYAD-", _flavor),
       string.concat("d", _flavor),
@@ -17,10 +25,14 @@ contract Factory {
     );
     DNftERC20 dNft = new DNftERC20(
       string.concat("DYAD NFT - ", _flavor),
-      string.concat("d", _flavor, "NFT"),
+      string.concat("d", _flavor, "-NFT"),
       address(dyad),
       _token,
       msg.sender
     );
+
+    dyad.transferOwnership(address(dNft));
+    deployed[_token][_oracle] = true;
+    emit Deployed(address(dNft), address(dyad));
   }
 }
